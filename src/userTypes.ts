f39902faae0776829fb92e57b152e174e8ebeee6
Hasib Muharam <hasibmuha@gmail.com>
2024-12-12 06:04:28 +0700

    initital project

---
+++ src/userTypes.ts
@@ -0,0 +1,10 @@
+export interface User {
+  user_id: number;
+  name: string;
+  email: string;
+  username: string;
+  gender: string;
+  password: string;
+  date_created: Date;
+  date_updated: Date;
+}
