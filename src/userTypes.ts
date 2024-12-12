005aa62f25d5e6fd708bff569931d44110fc6fb2
Hasib Muharam <hasibmuha@gmail.com>
2024-12-13 01:21:05 +0700

    update userTypes

--- src/userTypes.ts
+++ src/userTypes.ts
@@ -1,10 +1,12 @@
 export type User = {
-
-  document_id: string;
   name: string;
   username: string;
   email: string;
   gender: string;
   country: string;
-  
+}
+
+export type UserInputData = {
+  document_id: string;
+  data: User;
 }
